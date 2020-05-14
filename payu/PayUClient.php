b38d5f1d8440cec49af69ed233c5ab5464b2007b
Ashish Kumar <ashish.kumar@payu.in>
2020-05-14 14:49:49 +0530

    added php sdk code

---
+++ payu/PayUClient.php
@@ -0,0 +1,22 @@
+<?php
+
+include('base/payubase.php');
+/**
+ * key & salt values are provided by your AM from PayU side
+ */
+ $key = "";
+ $salt = "";
+class PayUClient
+{
+  function __construct($key,$salt)
+  {
+    $this->key = $key;
+    $this->salt = $salt;
+    $creds = base :: getInstance();
+    $creds->set($key,$salt);
+    $Array_var = (array) $creds;
+
+  }
+}
+
+ ?>
